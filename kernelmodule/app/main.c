ae3c6178f0f56524812c6062a28d8f8941d9735c
Resha Dwika Hefni Al-Fahsi <resha.dwika.hefni.alfahsi@mail.ugm.ac.id>
2019-07-18 11:30:00 +0700

    update kernel module

--- kernelmodule/app/main.c
+++ kernelmodule/app/main.c
@@ -2,25 +2,29 @@
 
 #define device "/dev/chardev"
 
+int i = 1;
+
 int main() {
 	FILE *fp;
 	char buffer[256];
 
 	printf("Demo calling kernel module using character device driver\n");
-	printf("Reading file %s...\n",device);
+	printf("Reading file %s...\n\n",device);
 
-	fp = fopen(device,"r");
-	if(fp == NULL) {
-		printf("Can't open file %s\n",device);
-		return 0;
+        for(i; i<=2; i++)
+        {
+          fp = fopen(device,"r");
+	  if(fp == NULL) {
+  		printf("Can't open file %s\n",device);
+  		return 0;
+	  }
+
+          fread(buffer,sizeof(buffer),1,fp);
+          if(i==1)printf("Nama: %s\n",buffer);
+          if(i==2)printf("NIM : %s\n",buffer);        
+ 
+          fclose(fp);
 	}
 	
-	fread(buffer,sizeof(buffer),1,fp);
-	printf("1. respond from kernel: %s\n",buffer);
-	
-	fread(buffer,sizeof(buffer),1,fp);
-	printf("2. respond from kernel: %s\n",buffer);
-
-	fclose(fp);
 	return 0;
 }
